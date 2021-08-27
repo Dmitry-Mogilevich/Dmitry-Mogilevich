f58aa1af07a5463d326a3b6919f8611b846c8520
Dmitry-Mogilevich <89597468+Dmitry-Mogilevich@users.noreply.github.com>
2021-08-27 20:33:51 +0300

    Store and update actions

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,1 +1,34 @@
 @extends('layout');
+
+@section('title', isset($user) ? 'Update '.$user->name : 'Create user')
+
+@section('content')
+    <a class="btn btn-danger" href="{{ route('users.index') }}">Back</a>
+    <form method="POST"
+          @if(isset($user))
+          action="{{ route('users.update', $user) }}"
+          @else
+          action="{{ route('users.store') }}"
+          @endif
+          class="mt-3">
+        @csrf
+        @isset($user)
+            @method('PUT')
+        @endisset
+        <div class="row">
+            <div class="col">
+                <input name="name" value="{{ isset($user) ? $user->name : null }}" type="text" class="form-control" placeholder="Name" aria-label="name">
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col">
+                <input name="email" value="{{ isset($user) ? $user->email : null }}" type="text" class="form-control" placeholder="Email" aria-label="email">
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col">
+                <button type="submit" class="btn btn-success">Create</button>
+            </div>
+        </div>
+    </form>
+@endsection

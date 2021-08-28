6d3ed761a3f209a997e711303757549ef05704ca
Dmitry-Mogilevich <89597468+Dmitry-Mogilevich@users.noreply.github.com>
2021-08-28 15:51:07 +0300

    Card for user

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,7 +1,19 @@
 @extends('layout');
 
-@section('title', 'Users')
+@section('title', 'User '.$user->name)
 
 @section('content')
-    {{ dd($user) }}
+    <div class="card" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Id: {{ $user->id }}</li>
+            <li class="list-group-item">Name: {{ $user->name }}</li>
+            <li class="list-group-item">Email: {{ $user->email }}</li>
+            <li class="list-group-item">Created: {{ $user->created_at->format('d/m/y H:i:s') }}</li>
+            <li class="list-group-item">Updated: {{ $user->updated_at->format('d/m/y H:i:s') }}</li>
+        </ul>
+    </div>
+    <div class="col mt-3">
+        <a class="btn btn-danger" href="{{ route('users.index') }}">Back</a>
+        <a class="btn btn-warning" type="button" href="{{ route('users.edit', $user) }}">Edit</a>
+    </div>
 @endsection
